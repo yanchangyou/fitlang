49818ca6a073fed115dd05e4e9a75b3236eb860b
imflyfish <yanchangyou@qq.com>
2023-09-01 21:25:42 +0800

    单元测试注释长时间sleep

--- src/test/java/fit/lang/plugin/json/tool/ServerJsonExecuteNodeTest.java
+++ src/test/java/fit/lang/plugin/json/tool/ServerJsonExecuteNodeTest.java
@@ -65,7 +65,7 @@ public class ServerJsonExecuteNodeTest extends TestCase {
         String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
 
         System.out.println(output);
-        Thread.sleep(1000 * 1000);
+//        Thread.sleep(1000 * 1000);
     }
 
 
