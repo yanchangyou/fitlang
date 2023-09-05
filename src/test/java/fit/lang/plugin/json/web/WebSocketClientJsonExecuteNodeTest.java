473ec446dff945c8efbd6eee47aafe363fde3e53
imflyfish <yanchangyou@qq.com>
2023-09-05 23:21:28 +0800

    删除send、receive节点

--- src/test/java/fit/lang/plugin/json/web/WebSocketClientJsonExecuteNodeTest.java
+++ src/test/java/fit/lang/plugin/json/web/WebSocketClientJsonExecuteNodeTest.java
@@ -1,5 +1,6 @@
 package fit.lang.plugin.json.web;
 
+import com.alibaba.fastjson2.JSONObject;
 import fit.lang.plugin.json.ExecuteJsonNodeUtil;
 import junit.framework.TestCase;
 
@@ -17,4 +18,43 @@ public class WebSocketClientJsonExecuteNodeTest extends TestCase {
         System.out.println(output);
 
     }
+
+    public void testServerAndClient() throws InterruptedException {
+
+        startWebSocketServer();
+        Thread.sleep(1000);
+
+        JSONObject message = new JSONObject();
+        message.put("who", System.currentTimeMillis());
+        message.put("uni", "hello");
+
+        startWebSocketClient(message);
+
+        Thread.sleep(1);
+
+    }
+
+    public void startWebSocketServer() {
+        String flow = "{" +//
+                "   'uni': 'wsServer'," +
+                "}";
+        String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
+
+        System.out.println(output);
+    }
+
+    public void startWebSocketClient(JSONObject message) {
+
+        String flow = "{" +//
+                "   'uni': 'wsClient'," +
+                "   'url': 'ws://127.0.0.1:10000/ws'," +
+                "}";
+
+        String output = ExecuteJsonNodeUtil.executeCode(message.toJSONString(), flow);
+
+        System.out.println(output);
+
+    }
+
+
 }
