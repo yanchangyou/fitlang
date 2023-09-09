ce619da95a3b0786854ba1877100de24582a0959
imflyfish <yanchangyou@qq.com>
2023-09-09 09:58:38 +0800

    systemInfo节点控制共享的字段

--- src/main/java/fit/lang/plugin/json/info/SystemBaseInfoJsonExecuteNode.java
+++ src/main/java/fit/lang/plugin/json/info/SystemBaseInfoJsonExecuteNode.java
@@ -1,5 +1,6 @@
 package fit.lang.plugin.json.info;
 
+import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import fit.lang.plugin.json.define.JsonExecuteNode;
 import fit.lang.plugin.json.define.JsonExecuteNodeInput;
@@ -18,6 +19,11 @@ public class SystemBaseInfoJsonExecuteNode extends JsonExecuteNode {
     @Override
     public void execute(JsonExecuteNodeInput input, JsonExecuteNodeOutput output) {
 
+        JSONArray shardFields = nodeJsonDefine.getJSONArray("shareFields");
+        if (shardFields == null) {
+            return;
+        }
+
         JSONObject systemInfoJson = new JSONObject();
 
         SystemInfo systemInfo = new SystemInfo();
@@ -26,41 +32,67 @@ public class SystemBaseInfoJsonExecuteNode extends JsonExecuteNode {
 
         //计算机
         ComputerSystem computerSystem = hardware.getComputerSystem();
-        String computerManufacturer = computerSystem.getManufacturer();//制造商
-        String computerModel = computerSystem.getModel();//型号
 
-        systemInfoJson.put("computerManufacturer", computerManufacturer);
-        systemInfoJson.put("computerModel", computerModel);
+        if (shardFields.contains("computerManufacturer")) {
+            String computerManufacturer = computerSystem.getManufacturer();//制造商
+            systemInfoJson.put("computerManufacturer", computerManufacturer);
+        }
+        if (shardFields.contains("computerModel")) {
+            String computerModel = computerSystem.getModel();//型号
+            systemInfoJson.put("computerModel", computerModel);
+        }
 
         //CPU
         CentralProcessor processor = hardware.getProcessor();
-        String processorName = processor.getProcessorIdentifier().getName();//处理名称
-        int physicalProcessorCount = processor.getPhysicalProcessorCount();//物理核心数
-        int logicalProcessorCount = processor.getLogicalProcessorCount();//逻辑核心数
-        long processorMaxFreq = processor.getMaxFreq();
+        if (shardFields.contains("processorName")) {
+            String processorName = processor.getProcessorIdentifier().getName();//处理名称
+            systemInfoJson.put("processorName", processorName);
+        }
+        if (shardFields.contains("processorPhysicalCount")) {
+            int physicalProcessorCount = processor.getPhysicalProcessorCount();//物理核心数
+            systemInfoJson.put("processorPhysicalCount", physicalProcessorCount);
+        }
+        if (shardFields.contains("processorLogicalCount")) {
+            int logicalProcessorCount = processor.getLogicalProcessorCount();//逻辑核心数
+            systemInfoJson.put("processorLogicalCount", logicalProcessorCount);
+        }
 
-        systemInfoJson.put("processorName", processorName);
-        systemInfoJson.put("processorPhysicalCount", physicalProcessorCount);
-        systemInfoJson.put("processorLogicalCount", logicalProcessorCount);
-        systemInfoJson.put("processorMaxFreq", covertToG(processorMaxFreq) + "G");
+        if (shardFields.contains("processorMaxFreq")) {
+            long processorMaxFreq = processor.getMaxFreq();
+            systemInfoJson.put("processorMaxFreq", covertToG(processorMaxFreq) + "G");
+        }
 
         //内存
         GlobalMemory memory = hardware.getMemory();
-        long memoryTotal = memory.getTotal();//内存大小
-        long memoryAvailable = memory.getAvailable();
-
-        systemInfoJson.put("memoryTotal", covertToG(memoryTotal) + "G");
-        systemInfoJson.put("memoryAvailable", covertToG(memoryAvailable) + "G");
+        if (shardFields.contains("memoryTotal")) {
+            long memoryTotal = memory.getTotal();//内存大小
+            systemInfoJson.put("memoryTotal", covertToG(memoryTotal) + "G");
+        }
+        if (shardFields.contains("memoryAvailable")) {
+            long memoryAvailable = memory.getAvailable();
+            systemInfoJson.put("memoryAvailable", covertToG(memoryAvailable) + "G");
+        }
 
         OperatingSystem os = systemInfo.getOperatingSystem();
-        int osBit = os.getBitness();
-        String osFamily = os.getFamily();
-        String osManufacturer = os.getManufacturer();
-        OperatingSystem.OSVersionInfo osVersion = os.getVersionInfo();
-        systemInfoJson.put("osManufacturer", osManufacturer);
-        systemInfoJson.put("osFamily", osFamily);
-        systemInfoJson.put("osVersion", osVersion.getVersion());
-        systemInfoJson.put("osBit", osBit);
+        if (shardFields.contains("osManufacturer")) {
+            String osManufacturer = os.getManufacturer();
+            systemInfoJson.put("osManufacturer", osManufacturer);
+        }
+
+        if (shardFields.contains("osFamily")) {
+            String osFamily = os.getFamily();
+            systemInfoJson.put("osFamily", osFamily);
+        }
+
+        if (shardFields.contains("osVersion")) {
+            OperatingSystem.OSVersionInfo osVersion = os.getVersionInfo();
+            systemInfoJson.put("osVersion", osVersion.getVersion());
+        }
+
+        if (shardFields.contains("osBit")) {
+            int osBit = os.getBitness();
+            systemInfoJson.put("osBit", osBit);
+        }
 
         output.setData(systemInfoJson);
 
