3eda7754ea90b5b38e0b308580894cf0d05cdbf9
imflyfish <yanchangyou@qq.com>
2023-09-10 18:14:00 +0800

    http单元测试

--- src/test/java/fit/lang/plugin/json/tool/HttpJsonExecuteNodeTest.java
+++ src/test/java/fit/lang/plugin/json/tool/HttpJsonExecuteNodeTest.java
@@ -1,7 +1,5 @@
 package fit.lang.plugin.json.tool;
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
 import fit.lang.plugin.json.ExecuteJsonNodeUtil;
 import junit.framework.TestCase;
 import org.junit.Assert;
@@ -10,23 +8,153 @@ public class HttpJsonExecuteNodeTest extends TestCase {
 
     public void testExecute() {
 
-        HttpJsonExecuteNode node = new HttpJsonExecuteNode();
         String flow = "{" +//
+                "   'uni':'http'," +
                 "   'url':'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic'," +
                 "   'header':{" +
                 "       'test':'123'" +
                 "   }" +
                 "}";
-        node.setNodeDefine(JSON.parseObject(flow));
-
-        String input = "{}";
-        JSONObject output = ExecuteJsonNodeUtil.execute(JSON.parseObject(input), node);
+        String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
 
         System.out.println(output);
 
         Assert.assertTrue(!output.isEmpty());
 
-        Assert.assertEquals("{\"error_code\":100,\"error_msg\":\"Invalid parameter\"}", output.toJSONString());
+        Assert.assertEquals("{'error_code':100,'error_msg':'Invalid parameter'}", output);
+
+    }
+
+
+    public void testExecute2() {
+
+        String flow = "{" +
+                "    'uni': 'sequence'," +
+                "    'child': [" +
+                "        {" +
+                "            'uni': 'server'," +
+                "            'port': 60000," +
+                "            'service': {" +
+                "                '/echo': {" +
+                "                    'uni': 'echo'" +
+                "                }" +
+                "            }" +
+                "        }," +
+                "        {" +
+                "            'uni': 'http'," +
+                "            'param': {" +
+                "                'hello': 'world'" +
+                "            }," +
+                "            'url': 'http://127.0.0.1:60000/echo'" +
+                "        }" +
+                "    ]" +
+                "}";
+
+        String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
+
+        System.out.println(output);
+
+        Assert.assertTrue(!output.isEmpty());
+
+        Assert.assertEquals("{'hello':'world'}", output);
+
+    }
+
+
+    public void testExecute3() {
+
+        String flow = "{" +
+                "    'uni': 'sequence'," +
+                "    'child': [" +
+                "        {" +
+                "            'uni': 'server'," +
+                "            'port': 60003," +
+                "            'service': {'/hello':{'uni':'hello'}}," +
+                "        }," +
+                "        {" +
+                "            'flag': '_needFormatJsonInConsole'," +
+                "            'uni': 'pipe'," +
+                "            'child': [" +
+                "                {" +
+                "                    'uni': 'mix'," +
+                "                    'json': {" +
+                "                        'path': '/opt/github/fitlang/doc/test/case/自动化测试用例.xls'," +
+                "                        'sheetName': '接口列表'" +
+                "                    }" +
+                "                }," +
+                "                {" +
+                "                    'uni': 'readExcel'," +
+                "                    'headerIndex': 1" +
+                "                }," +
+                "                {" +
+                "                    'uni': 'foreach'," +
+                "                    'foreachField': 'list'," +
+                "                    'child': {" +
+                "                        'uni': 'pipe'," +
+                "                        'child': [" +
+                "                            {" +
+                "                                'uni': 'set'," +
+                "                                'key': 'url'," +
+                "                                'value': '${接口URL}'" +
+                "                            }," +
+                "                            {" +
+                "                                'uni': 'mix'," +
+                "                                'json': {" +
+                "                                    'path': '/opt/github/fitlang/doc/test/case/自动化测试用例.xls'," +
+                "                                    'sheetName': '${接口名称}'" +
+                "                                }" +
+                "                            }," +
+                "                            {" +
+                "                                'uni': 'readExcel'," +
+                "                                'headerIndex': 1" +
+                "                            }," +
+                "                            {" +
+                "                                'uni': 'foreach'," +
+                "                                'foreachField': 'list'," +
+                "                                'child': {" +
+                "                                    'uni': 'pipe'," +
+                "                                    'child': [" +
+                "                                        {" +
+                "                                            'uni': 'set'," +
+                "                                            'key': 'expectedValue'," +
+                "                                            'value': '${期望}'" +
+                "                                        }," +
+                "                                        {" +
+                "                                            'uni': 'mix'," +
+                "                                            'json': {" +
+                "                                                'url': '${url}'," +
+                "                                                'expectedValue': '${expectedValue}'" +
+                "                                            }" +
+                "                                        }," +
+                "                                        {" +
+                "                                            'uni': 'http'," +
+                "                                            'header': {" +
+                "                                                'contextType': 'application/x-www-form-urlencoded'" +
+                "                                            }," +
+                "                                            'param': '${输入}'" +
+                "                                        }," +
+                "                                        {" +
+                "                                            'uni': 'assert'," +
+                "                                            'needToString': true," +
+                "                                            'expected': '${expectedValue}'" +
+                "                                        }" +
+                "                                    ]" +
+                "                                }" +
+                "                            }" +
+                "                        ]" +
+                "                    }" +
+                "                }" +
+                "            ]" +
+                "        }" +
+                "    ]" +
+                "}";
+
+        String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
+
+        System.out.println(output);
+
+        Assert.assertTrue(!output.isEmpty());
+
 
     }
 }
