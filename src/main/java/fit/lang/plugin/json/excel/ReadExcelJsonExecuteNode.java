c4fd5d238a0d651f903d903662a003e05ad87526
imflyfish <yanchangyou@qq.com>
2023-09-10 14:56:03 +0800

    excel：补回easyexcel，也支持xls，方便后续扩展

--- src/main/java/fit/lang/plugin/json/excel/ReadExcelJsonExecuteNode.java
+++ src/main/java/fit/lang/plugin/json/excel/ReadExcelJsonExecuteNode.java
@@ -3,18 +3,11 @@ package fit.lang.plugin.json.excel;
 import fit.lang.plugin.json.define.JsonExecuteNode;
 import fit.lang.plugin.json.define.JsonExecuteNodeInput;
 import fit.lang.plugin.json.define.JsonExecuteNodeOutput;
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static fit.lang.plugin.json.excel.util.EasyExcelUtil.readExcel;
 
 /**
  * 执行节点
@@ -38,28 +31,4 @@ public class ReadExcelJsonExecuteNode extends JsonExecuteNode {
         output.set("sheetData", list);
     }
 
-    private List<Map<String, String>> readExcel(String path, String sheetName, Integer headerIndex) throws IOException, BiffException {
-        InputStream is = Files.newInputStream(Paths.get(path));
-        jxl.Workbook rwb = Workbook.getWorkbook(is);
-
-        if (headerIndex == null) {
-            headerIndex = 0;
-        }
-        Sheet sheet = rwb.getSheet(0);
-        if (sheetName != null) {
-            sheet = rwb.getSheet(sheetName);
-        }
-        List<Map<String, String>> list = new ArrayList<>();
-        int rows = sheet.getRows();
-        for (int i = headerIndex; i < rows; i++) {
-            Map<String, String> row = new HashMap<>();
-            int column = sheet.getRow(i).length;
-            for (int j = 0; j < column; j++) {
-                row.put(j + "", sheet.getCell(j, i).getContents());
-            }
-            list.add(row);
-        }
-
-        return list;
-    }
 }
