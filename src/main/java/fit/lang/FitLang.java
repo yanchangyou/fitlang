9f107bd89458759d0c0c907d0c0c59aed9cd38cf
imflyfish <yanchangyou@qq.com>
2023-09-12 07:13:59 +0800

    0.4.13

--- src/main/java/fit/lang/FitLang.java
+++ src/main/java/fit/lang/FitLang.java
@@ -8,6 +8,6 @@ public class FitLang {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.4.12";
+    public static final String VERSION = "0.4.13";
 
 }
--- src/main/java/fit/server/FitServerMain.java
+++ src/main/java/fit/server/FitServerMain.java
@@ -16,7 +16,7 @@ public class FitServerMain {
 
     public static void main(String[] args) {
 
-        System.out.println("FitLang-0.4.12");
+        System.out.println("FitLang-0.4.13");
         String serverFilePath = "server.fit";
         String httpPrefix = "http://127.0.0.1";
         int port = 11111;
