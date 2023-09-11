06815c29422652d8de9c6b34b7de6f0fb8f35e85
imflyfish <yanchangyou@qq.com>
2023-09-11 22:17:32 +0800

    set节点去掉 ${this}支持，会导致结构循环，导致死循环

--- src/main/java/fit/lang/plugin/json/util/SetJsonExecuteNode.java
+++ src/main/java/fit/lang/plugin/json/util/SetJsonExecuteNode.java
@@ -15,12 +15,7 @@ public class SetJsonExecuteNode extends JsonExecuteNode {
         String key = nodeJsonDefine.getString("key");
         Object value = nodeJsonDefine.get("value");
 
-        Object newValue;
-        if ("${this}".equals(value)) {
-            newValue = input.getData();
-        } else {
-            newValue = ExpressUtil.eval(value, input.getInputParamAndContextParam());
-        }
+        Object newValue = ExpressUtil.eval(value, input.getInputParamAndContextParam());
 
         input.getNodeContext().setAttribute(key, newValue);
 
