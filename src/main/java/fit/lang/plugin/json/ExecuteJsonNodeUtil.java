cc8c7464834ecd77a35c4a6b40a4a4b5a246032e
imflyfish <yanchangyou@qq.com>
2023-09-14 23:01:31 +0800

    执行引擎，把入参放入上下文中

--- src/main/java/fit/lang/plugin/json/ExecuteJsonNodeUtil.java
+++ src/main/java/fit/lang/plugin/json/ExecuteJsonNodeUtil.java
@@ -127,6 +127,7 @@ public class ExecuteJsonNodeUtil {
             input = new JSONObject();
         }
 
+        contextParam.putAll(input);
         input = eval(input, contextParam);
 
         //入参放入全局变量中
