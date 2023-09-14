a287f02ce538d80683e5063b6d7e743a958c344b
imflyfish <yanchangyou@qq.com>
2023-09-14 22:40:20 +0800

    excel读取配置信息时，使用统一的方法；单元测试完善

--- src/main/java/fit/lang/plugin/json/excel/WriteExcelJsonExecuteNode.java
+++ src/main/java/fit/lang/plugin/json/excel/WriteExcelJsonExecuteNode.java
@@ -4,7 +4,6 @@ import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import fit.lang.ExecuteNodeException;
-import fit.lang.plugin.json.ExpressUtil;
 import fit.lang.plugin.json.define.JsonExecuteNode;
 import fit.lang.plugin.json.define.JsonExecuteNodeInput;
 import fit.lang.plugin.json.define.JsonExecuteNodeOutput;
@@ -24,28 +23,17 @@ public class WriteExcelJsonExecuteNode extends JsonExecuteNode {
     @Override
     public void execute(JsonExecuteNodeInput input, JsonExecuteNodeOutput output) {
 
-        String path = nodeJsonDefine.getString("path");
         Integer defaultWidth = nodeJsonDefine.getInteger("defaultWidth");
         String listField = nodeJsonDefine.getString(FIELD_NAME_OF_INPUT_LIST_FIELD);
         JSONObject titleConfig = nodeJsonDefine.getJSONObject("titleConfig");
-
-        String sheetName = nodeJsonDefine.getString("sheetName");
-        if (StrUtil.isBlank(sheetName)) {
-            sheetName = input.getString("sheetName");
-        }
 
         if (StrUtil.isBlank(listField)) {
             listField = "list";
         }
 
-        if (StrUtil.isBlank(path)) {
-            path = input.getString("path");
-        }
-
-        //支持表达式
-        path = (String) ExpressUtil.eval(path, input.getInputParamAndContextParam());
-
-        path = buildFilePath(path);
+        String sheetName = parseStringField("sheetName", input);
+        String path = parseStringField("path", input);
+        path = buildFilePath(path, "readExcel");
 
         JSONArray list = input.getJsonArray(listField);
 
