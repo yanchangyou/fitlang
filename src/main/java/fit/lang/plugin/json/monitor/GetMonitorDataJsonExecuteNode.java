2f3f278578de4f2b9e4e9625d1c9df5bb498f54f
imflyfish <yanchangyou@qq.com>
2023-09-16 21:41:45 +0800

    监控：支持获取最近N秒数据,默认返回10分钟，600秒

--- src/main/java/fit/lang/plugin/json/monitor/GetMonitorDataJsonExecuteNode.java
+++ src/main/java/fit/lang/plugin/json/monitor/GetMonitorDataJsonExecuteNode.java
@@ -1,5 +1,6 @@
 package fit.lang.plugin.json.monitor;
 
+import cn.hutool.core.util.NumberUtil;
 import com.alibaba.fastjson2.JSONObject;
 import fit.lang.plugin.json.define.JsonExecuteNode;
 import fit.lang.plugin.json.define.JsonExecuteNodeInput;
@@ -15,9 +16,17 @@ public class GetMonitorDataJsonExecuteNode extends JsonExecuteNode {
     @Override
     public void execute(JsonExecuteNodeInput input, JsonExecuteNodeOutput output) {
 
-        List<JSONObject> cpuGatherList = StartMonitorJsonExecuteNode.getCpuGatherList();
+        String secondText = parseStringField("second", input);
+
+        int second = 600;
+        if (secondText != null && NumberUtil.isInteger(secondText)) {
+            second = Integer.parseInt(secondText);
+        }
+
+        List<JSONObject> cpuGatherList = StartMonitorJsonExecuteNode.getCpuGatherList(second);
         JSONObject result = new JSONObject();
         result.put("cpu", cpuGatherList);
+        result.put("second", second);
 
         output.setData(result);
     }
