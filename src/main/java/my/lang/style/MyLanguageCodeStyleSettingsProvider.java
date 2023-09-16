e6fa59a3e85dafef447dfe0e854cd8f1f1da9a66
imflyfish <yanchangyou@qq.com>
2023-09-16 22:12:09 +0800

    删除多余的NotNull

--- src/main/java/my/lang/style/MyLanguageCodeStyleSettingsProvider.java
+++ src/main/java/my/lang/style/MyLanguageCodeStyleSettingsProvider.java
@@ -1,16 +1,6 @@
 package my.lang.style;
 
 import com.intellij.json.formatter.JsonLanguageCodeStyleSettingsProvider;
-import my.lang.MyLanguage;
-import com.intellij.application.options.CodeStyleAbstractConfigurable;
-import com.intellij.application.options.CodeStyleAbstractPanel;
-import com.intellij.application.options.TabbedLanguageCodeStylePanel;
-import com.intellij.psi.codeStyle.CodeStyleConfigurable;
-import com.intellij.psi.codeStyle.CodeStyleSettings;
-import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
-import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author yanchangyou
--- src/main/java/my/lang/style/MyLanguageLanguageCodeStyleSettingsProvider.java
+++ src/main/java/my/lang/style/MyLanguageLanguageCodeStyleSettingsProvider.java
@@ -1,11 +1,6 @@
 package my.lang.style;
 
 import com.intellij.json.formatter.JsonLanguageCodeStyleSettingsProvider;
-import my.lang.MyLanguage;
-import com.intellij.lang.Language;
-import com.intellij.psi.codeStyle.CodeStyleSettingsCustomizable;
-import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author yanchangyou
