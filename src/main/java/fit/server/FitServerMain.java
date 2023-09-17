ab4cbbe8de58a86b7c881d6db5990f3c9a024ae1
imflyfish <yanchangyou@qq.com>
2023-09-17 20:30:07 +0800

    0.5.6

--- src/main/java/fit/server/FitServerMain.java
+++ src/main/java/fit/server/FitServerMain.java
@@ -14,7 +14,7 @@ public class FitServerMain {
 
     public static void main(String[] args) {
 
-        System.out.println("FitLang-0.5.5");
+        System.out.println("FitLang-0.5.6");
         String serverFilePath = "server.fit";
         String httpPrefix = "http://127.0.0.1";
         int port = 11111;
