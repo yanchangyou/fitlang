128578faaef573acd9e6d59834221b3eb59c8de5
imflyfish <yanchangyou@qq.com>
2023-09-24 18:25:22 +0800

    空linux安装步骤

--- fitlang-springboot/bin/install.sh
+++ fitlang-springboot/bin/install.sh
@@ -3,3 +3,10 @@ yum install git java unzip lsof -y
 
 yum install https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-7/x86_64/apache-maven-3.5.2-1.el7.noarch.rpm -y
 
+git clone https://gitee.com/imflyfish/fitlang
+
+cd fitlang/fitlang-springboot
+
+wget https://gitee.com/imflyfish/fitlang/raw/serverless/fitlang-springboot/doc/settings.xml -O ~/.m2/settings.xml
+
+bin/start.sh
