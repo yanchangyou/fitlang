db5051bec3ce7400a22c3ecb4615778f3b2dc1c9
imflyfish <yanchangyou@qq.com>
2023-10-27 23:33:59 +0800

    编辑器uni节点智能补全

--- fitlang-plugin-idea-json/src/fit/intellij/json/codeinsight/JsonCompletionContributor.java
+++ fitlang-plugin-idea-json/src/fit/intellij/json/codeinsight/JsonCompletionContributor.java
@@ -32,30 +32,43 @@ import static com.intellij.patterns.PlatformPatterns.psiElement;
  */
 public class JsonCompletionContributor extends CompletionContributor {
 
-  private static final PsiElementPattern.Capture<PsiElement> AFTER_COLON_IN_PROPERTY = psiElement()
-    .afterLeaf(":").withSuperParent(2, JsonProperty.class)
-    .andNot(psiElement().withParent(JsonStringLiteral.class));
-  
-  private static final PsiElementPattern.Capture<PsiElement> AFTER_COMMA_OR_BRACKET_IN_ARRAY = psiElement()
-    .afterLeaf("[", ",").withSuperParent(2, JsonArray.class)
-    .andNot(psiElement().withParent(JsonStringLiteral.class));
-  
-  public JsonCompletionContributor() {
-    extend(CompletionType.BASIC, AFTER_COLON_IN_PROPERTY, MyKeywordsCompletionProvider.INSTANCE);
-    extend(CompletionType.BASIC, AFTER_COMMA_OR_BRACKET_IN_ARRAY, MyKeywordsCompletionProvider.INSTANCE);
-  }
+    private static final PsiElementPattern.Capture<PsiElement> AFTER_COLON_IN_PROPERTY = psiElement().afterLeaf(":").withSuperParent(2, JsonProperty.class).andNot(psiElement().withParent(JsonStringLiteral.class));
 
-  private static class MyKeywordsCompletionProvider extends CompletionProvider<CompletionParameters> {
-    private static final MyKeywordsCompletionProvider INSTANCE = new MyKeywordsCompletionProvider();
-    private static final String[] KEYWORDS = new String[]{"null", "true", "false"};
+    private static final PsiElementPattern.Capture<PsiElement> AFTER_COMMA_OR_BRACKET_IN_ARRAY = psiElement().afterLeaf("[", ",").withSuperParent(2, JsonArray.class).andNot(psiElement().withParent(JsonStringLiteral.class));
 
-    @Override
-    protected void addCompletions(@NotNull CompletionParameters parameters,
-                                  @NotNull ProcessingContext context,
-                                  @NotNull CompletionResultSet result) {
-      for (String keyword : KEYWORDS) {
-        result.addElement(LookupElementBuilder.create(keyword).bold());
-      }
+
+    private static final PsiElementPattern.Capture<PsiElement> UNI_KEY_WORD = psiElement().afterLeaf("{");
+
+
+    public JsonCompletionContributor() {
+        extend(CompletionType.BASIC, AFTER_COLON_IN_PROPERTY, MyKeywordsCompletionProvider.INSTANCE);
+        extend(CompletionType.BASIC, AFTER_COMMA_OR_BRACKET_IN_ARRAY, MyKeywordsCompletionProvider.INSTANCE);
+        extend(CompletionType.BASIC, UNI_KEY_WORD, UniKeywordsCompletionProvider.INSTANCE);
     }
-  }
+
+    private static class MyKeywordsCompletionProvider extends CompletionProvider<CompletionParameters> {
+        private static final MyKeywordsCompletionProvider INSTANCE = new MyKeywordsCompletionProvider();
+        private static final String[] KEYWORDS = new String[]{"null", "true", "false"};
+
+        @Override
+        protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
+            for (String keyword : KEYWORDS) {
+                result.addElement(LookupElementBuilder.create(keyword).bold());
+            }
+        }
+    }
+
+
+    private static class UniKeywordsCompletionProvider extends CompletionProvider<CompletionParameters> {
+        private static final UniKeywordsCompletionProvider INSTANCE = new UniKeywordsCompletionProvider();
+
+        final static String keywords[] = {"hello", "increase", "decrease", "echo", "assert", "set", "sleep", "print", "timeCount", "add", "convert", "removeField", "removeEmptyField", "mix", "mixNode", "eval", "parseJson", "stringifyJson", "convertKeyValueList", "sequence", "pipe", "foreach", "loop", "switch", "return", "thread", "execute", "call", "gitPull", "http", "postJson", "postForm", "httpGet", "httpPut", "httpDelete", "server", "proxy", "web", "wsServer", "wsClient", "readExcel", "writeExcel", "readFile", "writeFile", "deleteFile", "systemInfo", "startMonitor", "getMonitorData", "getClientMonitorData", "receiveClientMonitorData", "pushClientMonitorData", "getMonitorClient", "cloudServer", "cloudClient"};
+
+        @Override
+        protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
+            for (String keyword : keywords) {
+                result.addElement(LookupElementBuilder.create("uni\":\"".concat(keyword)).bold());
+            }
+        }
+    }
 }
