cc42134d4a8e2ad1d12d0e5665bb35dd628a915a
imflyfish <yanchangyou@qq.com>
2023-10-28 17:41:07 +0800

    0.6.4

--- fitlang-server/src/main/java/fit/server/FitServerMain.java
+++ fitlang-server/src/main/java/fit/server/FitServerMain.java
@@ -14,7 +14,7 @@ public class FitServerMain {
 
     public static void main(String[] args) {
 
-        System.out.println("FitLang-0.6.3");
+        System.out.println("FitLang-0.6.4");
         String fitPath = "app";
         String serverFilePath = "server.fit";
         String httpPrefix = "http://127.0.0.1";
--- fitlang-springboot/bootstrap.sh
+++ fitlang-springboot/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.6.3-SNAPSHOT.jar
+java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.6.4-SNAPSHOT.jar
