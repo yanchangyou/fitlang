4113bc53a35a529babe7bc7da6e86997c7cfeb8a
imflyfish <yanchangyou@qq.com>
2023-11-24 23:00:41 +0800

    fastjson2升级：2.0.42 hutool升级：5.8.23 oshi升级：6.4.7

--- fitlang-server/demo/fitserver/jdk/run.sh
+++ fitlang-server/demo/fitserver/jdk/run.sh
@@ -10,12 +10,12 @@ curl http://127.0.0.1:11111/_shutdown
 sleep 0.5
 
 jdk/bin/java -classpath lib/fitlang-0.7.3.jar:\
-lib/hutool-all-5.8.21.jar:\
+lib/hutool-all-5.8.23.jar:\
 lib/aviator-5.3.3.jar:\
 lib/jna-5.13.0.jar:\
 lib/jna-platform-5.13.0.jar:\
-lib/oshi-core-6.4.5.jar:\
-lib/fastjson2-2.0.32.jar:\
+lib/oshi-core-6.4.7.jar:\
+lib/fastjson2-2.0.42.jar:\
 lib/slf4j-api-2.0.7.jar:\
  fit.server.FitServerMain server.fit $httpPrefix $fitPath&
 
