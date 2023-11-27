91d0cfe3908c83838bf135fd9016f40d3f89abd2
imflyfish <yanchangyou@qq.com>
2023-11-27 22:01:30 +0800

    0.7.4

--- fitlang-server/demo/fitserver/run.bat
+++ fitlang-server/demo/fitserver/run.bat
@@ -1,4 +1,4 @@
 
 cd /d %~dp0
 
-jdk\bin\java -classpath lib/fitlang-0.7.3.jar;lib/slf4j-api-2.0.9.jar;jcl-over-slf4j-2.0.9.jar;lib/oshi-core-6.4.7.jar;lib/jna-platform-5.13.0.jar;lib/jna-5.13.0.jar;lib/hutool-all-5.8.23.jar;lib/fastjson2-2.0.42.jar;lib/aviator-5.3.3.jar fit.server.FitServerMain
+jdk\bin\java -classpath lib/fitlang-0.7.4.jar;lib/slf4j-api-2.0.9.jar;jcl-over-slf4j-2.0.9.jar;lib/oshi-core-6.4.7.jar;lib/jna-platform-5.13.0.jar;lib/jna-5.13.0.jar;lib/hutool-all-5.8.23.jar;lib/fastjson2-2.0.42.jar;lib/aviator-5.3.3.jar fit.server.FitServerMain
