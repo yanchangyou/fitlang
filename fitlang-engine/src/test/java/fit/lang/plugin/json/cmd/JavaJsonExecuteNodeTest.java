294f1dbafe99e7419258bd3d8f11120c8df10f0c
imflyfish <yanchangyou@qq.com>
2023-12-01 23:19:16 +0800

    添加java命令

---
+++ fitlang-engine/src/test/java/fit/lang/plugin/json/cmd/JavaJsonExecuteNodeTest.java
@@ -0,0 +1,31 @@
+package fit.lang.plugin.json.cmd;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import fit.lang.plugin.json.ExecuteJsonNodeUtil;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class JavaJsonExecuteNodeTest extends TestCase {
+
+    public void testExecuteWithParam2() {
+        String flow = "{" +//
+                "   'uni': 'java'," +
+                "   'param': {" +
+                "       '-version': ''," +
+                "       '-XX:': '+PrintGCDetails'," +
+                "   }" +
+                "}";
+
+        String output = ExecuteJsonNodeUtil.executeCode("{}", flow);
+
+        JSONObject outputJson = JSON.parseObject(output);
+
+        Assert.assertTrue(!output.isEmpty());
+
+        System.out.println(output);
+
+        Assert.assertTrue(outputJson.containsKey("result"));
+
+    }
+}
