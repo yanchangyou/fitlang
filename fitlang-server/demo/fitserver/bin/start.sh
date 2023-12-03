5ef9457d379fbbdce1d785091065055b76627531
imflyfish <yanchangyou@qq.com>
2023-12-03 22:43:37 +0800

    aviator替换为mvel同步修改

--- fitlang-server/demo/fitserver/bin/start.sh
+++ fitlang-server/demo/fitserver/bin/start.sh
@@ -11,7 +11,7 @@ sleep 0.5
 
 java -classpath lib/fitlang-0.7.6.jar:\
 lib/hutool-all-5.8.23.jar:\
-lib/aviator-5.3.3.jar:\
+lib/mvel2-2.5.1.Final.jar:\
 lib/jna-5.13.0.jar:\
 lib/jna-platform-5.13.0.jar:\
 lib/oshi-core-6.4.7.jar:\
