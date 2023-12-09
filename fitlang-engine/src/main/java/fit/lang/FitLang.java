6bd31f2417f1469db09c01ca15e30316adfe3a2d
imflyfish <yanchangyou@qq.com>
2023-12-09 15:26:13 +0800

    0.7.8

--- fitlang-engine/src/main/java/fit/lang/FitLang.java
+++ fitlang-engine/src/main/java/fit/lang/FitLang.java
@@ -8,6 +8,6 @@ public class FitLang {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.7.7";
+    public static final String VERSION = "0.7.8";
 
 }
--- fitlang-springboot/bootstrap.sh
+++ fitlang-springboot/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.7.7-SNAPSHOT.jar
+java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.7.8-SNAPSHOT.jar
