46b28710db0ca2b7c81bb457d28b5f2fb9ca8444
imflyfish <yanchangyou@qq.com>
2023-12-23 23:59:40 +0800

    getField改名为get，JSON Path是一等公民

---
+++ fitlang-engine/src/main/java/fit/lang/plugin/json/json/GetJsonExecuteNode.java
@@ -0,0 +1,29 @@
+package fit.lang.plugin.json.json;
+
+import com.alibaba.fastjson2.JSONObject;
+import fit.lang.plugin.json.define.JsonExecuteNode;
+import fit.lang.plugin.json.define.JsonExecuteNodeInput;
+import fit.lang.plugin.json.define.JsonExecuteNodeOutput;
+
+/**
+ * 执行节点
+ */
+public class GetJsonExecuteNode extends JsonExecuteNode {
+
+    @Override
+    public void execute(JsonExecuteNodeInput input, JsonExecuteNodeOutput output) {
+
+        String path = nodeJsonDefine.getString("path");
+        Object object = input.getInputParamAndContextParam().getByPath(path);
+        JSONObject data;
+
+        if (object instanceof JSONObject) {
+            data = (JSONObject) object;
+        } else {
+            data = new JSONObject(1);
+            data.put("value", object);
+        }
+
+        output.setData(data);
+    }
+}
