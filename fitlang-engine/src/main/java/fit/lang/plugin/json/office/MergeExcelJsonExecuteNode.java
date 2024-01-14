281166d174a7aad2e5ffcc57037968745efa2c7b
imflyfish <yanchangyou@qq.com>
2024-01-14 20:36:49 +0800

    合并excel，如果outputFile为空，就默认为第一个文件同路径，使用时间戳作为文件名

--- fitlang-engine/src/main/java/fit/lang/plugin/json/office/MergeExcelJsonExecuteNode.java
+++ fitlang-engine/src/main/java/fit/lang/plugin/json/office/MergeExcelJsonExecuteNode.java
@@ -1,13 +1,16 @@
 package fit.lang.plugin.json.office;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import fit.lang.plugin.json.define.JsonExecuteNode;
 import fit.lang.plugin.json.define.JsonExecuteNodeInput;
 import fit.lang.plugin.json.define.JsonExecuteNodeOutput;
 
+import java.io.File;
 import java.util.List;
 
+import static fit.lang.ExecuteNodeUtil.getNow;
 import static fit.lang.plugin.json.office.NodeExcelUtil.readExcel;
 import static fit.lang.plugin.json.office.NodeExcelUtil.writeExcel;
 
@@ -21,7 +24,15 @@ public class MergeExcelJsonExecuteNode extends JsonExecuteNode {
 
         List<String> inputFiles = parseStringArray("inputFiles", input);
 
+        if (inputFiles == null && inputFiles.isEmpty()) {
+            output.set("inputFiles", inputFiles);
+            return;
+        }
+
         String outputFile = parseStringField("outputFile", input);
+        if (StrUtil.isBlank(outputFile)) {
+            outputFile = new File(inputFiles.get(0)).getParent() + File.separator + "merge-" + getNow("yyyyMMddHHmmss") + ".xls";
+        }
 
         String sheetName = nodeJsonDefine.getString("sheetName");
 
