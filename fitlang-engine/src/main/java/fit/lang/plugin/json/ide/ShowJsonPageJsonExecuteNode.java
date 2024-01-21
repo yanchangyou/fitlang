e47c9438ac33c9b26bd860c2b03c1d1c0fa78e06
imflyfish <yanchangyou@qq.com>
2024-01-21 19:28:16 +0800

    json page支持默认参数

--- fitlang-engine/src/main/java/fit/lang/plugin/json/ide/ShowJsonPageJsonExecuteNode.java
+++ fitlang-engine/src/main/java/fit/lang/plugin/json/ide/ShowJsonPageJsonExecuteNode.java
@@ -13,14 +13,24 @@ public class ShowJsonPageJsonExecuteNode extends JsonExecuteNode {
     @Override
     public void execute(JsonExecuteNodeInput input, JsonExecuteNodeOutput output) {
 
-        JSONObject jsonPage = nodeJsonDefine.getJSONObject("page");
+        Object page = parseField("page", input);
         Object data = parseField("data", input);
+
         JSONObject jsonData;
         if (data instanceof JSONObject) {
             jsonData = (JSONObject) data;
         } else {
             jsonData = new JSONObject();
             jsonData.put("data", data);
+        }
+
+        JSONObject jsonPage;
+        if (page instanceof JSONObject) {
+            jsonPage = (JSONObject) page;
+        } else {
+            jsonPage = new JSONObject();
+            jsonPage.put("type", "page");
+            jsonPage.put("title", "Json Page");
         }
 
         JSONObject option = nodeJsonDefine.getJSONObject("option");
