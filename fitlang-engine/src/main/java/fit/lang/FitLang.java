be1bcab227e532786d75444af047ee53e928c8d3
imflyfish <yanchangyou@qq.com>
2024-01-26 20:37:28 +0800

    0.10.2

--- fitlang-engine/src/main/java/fit/lang/FitLang.java
+++ fitlang-engine/src/main/java/fit/lang/FitLang.java
@@ -8,6 +8,6 @@ public class FitLang {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.10.1";
+    public static final String VERSION = "0.10.2";
 
 }
--- fitlang-server/demo/fitserver/run.bat
+++ fitlang-server/demo/fitserver/run.bat
@@ -1,4 +1,4 @@
 
 cd /d %~dp0
 
-java -classpath lib/fitlang-0.10.1.jar;lib/slf4j-api-2.0.9.jar;lib/slf4j-jdk14-2.0.9.jar;lib/jul-to-slf4j-2.0.9.jar;lib/oshi-core-6.4.7.jar;lib/jna-platform-5.13.0.jar;lib/jna-5.13.0.jar;lib/hutool-all-5.8.23.jar;lib/fastjson2-2.0.42.jar;lib/mvel2-2.5.1.Final.jar fit.server.FitServerMain
+java -classpath lib/fitlang-0.10.2.jar;lib/slf4j-api-2.0.9.jar;lib/slf4j-jdk14-2.0.9.jar;lib/jul-to-slf4j-2.0.9.jar;lib/oshi-core-6.4.7.jar;lib/jna-platform-5.13.0.jar;lib/jna-5.13.0.jar;lib/hutool-all-5.8.23.jar;lib/fastjson2-2.0.42.jar;lib/mvel2-2.5.1.Final.jar fit.server.FitServerMain
