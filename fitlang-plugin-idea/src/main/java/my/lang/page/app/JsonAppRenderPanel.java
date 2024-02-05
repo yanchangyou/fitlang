2d02ffd05fa5791569417d6b4df921a0499581c6
imflyfish <yanchangyou@qq.com>
2024-02-05 21:40:52 +0800

    toolbar高度优化

--- fitlang-plugin-idea/src/main/java/my/lang/page/app/JsonAppRenderPanel.java
+++ fitlang-plugin-idea/src/main/java/my/lang/page/app/JsonAppRenderPanel.java
@@ -156,7 +156,7 @@ public class JsonAppRenderPanel extends JPanel {
         }
 
         JBScrollPane jbScrollPane = new JBScrollPane(toolBar);
-        Dimension dimension = new Dimension(toolBar.getWidth(), toolBar.getHeight() + 65);
+        Dimension dimension = new Dimension(toolBar.getWidth(), toolBar.getHeight() + 60);
         jbScrollPane.setPreferredSize(dimension);
         jbScrollPane.setBorder(JBUI.Borders.empty(5));
         panel.add(jbScrollPane, BorderLayout.NORTH);
