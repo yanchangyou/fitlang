c7438412f47b995074c803f4329a56f7f9e636c7
imflyfish <yanchangyou@qq.com>
2024-02-27 23:33:17 +0800

    0.10.15

--- fitlang-engine/src/main/java/fit/lang/FitLang.java
+++ fitlang-engine/src/main/java/fit/lang/FitLang.java
@@ -8,6 +8,6 @@ public class FitLang {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.10.14";
+    public static final String VERSION = "0.10.15";
 
 }
--- fitlang-springboot/bootstrap.sh
+++ fitlang-springboot/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.10.14-SNAPSHOT.jar
+java -Dserver.port=9000 -jar ./target/fitlang-springboot-0.10.15-SNAPSHOT.jar
